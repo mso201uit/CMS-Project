de22959de060b909c89a341f3266b4d03aefbed8
Sas <ssa171@post.uit.no>
2024-11-05 20:50:17 +0100

    update models

---
+++ Models/ContentType.cs
@@ -0,0 +1,14 @@
+﻿namespace CMS_Project.Models;
+
+public class ContentType
+{
+    public int Id { get; set; }
+    public string Type { get; set; }
+    
+    public ICollection<Document> Documents { get; set; }
+
+    public ContentType()
+    {
+        Documents = new List<Document>();
+    }
+}
